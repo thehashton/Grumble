503d92424724569ec43cf444e67d6c9b7805e194
Harry Ghazni <hghazni@gmail.com>
2019-02-09 05:19:14 +0000

    Tidies up and re-organises project.

--- graphql/index.js
+++ graphql/index.js
@@ -1,11 +1,38 @@
-import { makeExecutableSchema } from "graphql-tools";
+import express from "express";
+import expressGraphQL from "express-graphql";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+const app = express();
+const opn = require('opn');
+const PORT = process.env.PORT || "4000";
+const db = "mongodb://harry:Sophie777@ds052968.mlab.com:52968/lunch";
+import schema from "./schema/index";
 
-import typeDefs from "./types/";
-import resolvers from "./resolvers/";
 
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
-});
+// Connect to MongoDB with Mongoose.
+mongoose
+  .connect(
+    db,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-export default schema;
+  app.use(
+    "/graphql",
+    cors(),
+    bodyParser.json(),
+    expressGraphQL({
+      schema,
+      graphiql: true
+    })
+  );
+  
+  app.listen(PORT, () => 
+    console.log(`Server running on port localhost:${PORT}/graphql`),
+    opn('http://localhost:4000/graphql')
+  );

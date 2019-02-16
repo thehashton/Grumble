df6cb5de8266149290cba6103f3eae531763b591
Harry Ghazni <hghazni@gmail.com>
2019-02-16 19:41:13 +0000

    Adds basic styling for home including a placeholder logo/image

--- src/RootComponents/Home/Home.tsx
+++ src/RootComponents/Home/Home.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import Buttons from '../../components/Home/Buttons';
+import './home.scss';
 
 export default class Home extends Component {
+
   render() {
     return (
-      <div>
-        <h1>Find a place to eat</h1>
+      <div className="home-wrapper">
+      <img src="../assets/images/noodles.png" alt="Place to Eat" className="logo"/>
+        <h1>Hungry?</h1>
+        <p>Let's find you a place to eat</p>
         <Buttons />
       </div>
     )

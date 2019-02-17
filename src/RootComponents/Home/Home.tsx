24cf22d9ce96473288be9f3ee01e8ce7307f4a48
Harry Ghazni <hghazni@gmail.com>
2019-02-17 02:43:10 +0000

    Adds random hero title on page load via react refs after render

--- src/RootComponents/Home/Home.tsx
+++ src/RootComponents/Home/Home.tsx
@@ -2,16 +2,29 @@ import React, { Component } from 'react'
 import Buttons from '../../components/Home/Buttons';
 import './home.scss';
 
-export default class Home extends Component {
+export default class Home extends Component { 
+
+  private heroTitle: null | HTMLDivElement = null; // hero title ref
+
+  componentDidMount() {
+    var msgArr = new Array();
+  
+    msgArr[0] = "Hungry?";
+    msgArr[1] = "Famished?";
+    msgArr[2] = "You a starvin marvin?";
+      if (this.heroTitle) {
+        this.heroTitle.innerHTML = msgArr[Math.floor(Math.random()*msgArr.length)];
+      }
+ }
 
   render() {
     return (
       <div className="home-wrapper">
       <img src="../assets/images/noodles.png" alt="Place to Eat" className="logo"/>
-        <h1>Hungry?</h1>
+        <h1 id="hero-title" ref={c => (this.heroTitle = c as HTMLDivElement)}>Hungry?</h1>
         <p>Let's find you a place to eat</p>
         <Buttons />
       </div>
     )
   }
-}
+};

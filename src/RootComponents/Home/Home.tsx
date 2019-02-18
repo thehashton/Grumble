a86d21b19748e7a3e83c6ce4e86db7421202c1c4
Harry Ghazni <hghazni@gmail.com>
2019-02-18 15:32:19 +0000

    Converted random title to stateless component

--- src/RootComponents/Home/Home.tsx
+++ src/RootComponents/Home/Home.tsx
@@ -1,30 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Buttons from '../../components/Home/Buttons';
 import './home.scss';
 
-export default class Home extends Component { 
+export const Home: React.SFC = () => { 
 
-  private heroTitle: null | HTMLDivElement = null; // hero title ref
+  var msgArr = new Array();
 
-  componentDidMount() {
-    var msgArr = new Array();
-  
-    msgArr[0] = "Hungry?";
-    msgArr[1] = "Famished?";
-    msgArr[2] = "You a starvin marvin?";
-      if (this.heroTitle) {
-        this.heroTitle.innerHTML = msgArr[Math.floor(Math.random()*msgArr.length)];
-      }
- }
+  msgArr[0] = "Hungry?";
+  msgArr[1] = "Famished?";
+  msgArr[2] = "You a starvin marvin?";
 
-  render() {
-    return (
-      <div className="home-wrapper">
-      <img src="../assets/images/noodles.png" alt="Place to Eat" className="logo"/>
-        <h1 id="hero-title" ref={c => (this.heroTitle = c as HTMLDivElement)}>Hungry?</h1>
-        <p>Let's find you a place to eat</p>
-        <Buttons />
-      </div>
-    )
-  }
+  const message = msgArr[Math.floor(Math.random()*msgArr.length)];
+
+  return (
+    <div className="home-wrapper">
+    <img src="../assets/images/noodles.png" alt="Place to Eat" className="logo"/>
+      <h1 id="hero-title">{message}</h1>
+      <p>Let's find you a place to eat</p>
+      <Buttons />
+    </div>
+  )
 };
+
+export default Home;

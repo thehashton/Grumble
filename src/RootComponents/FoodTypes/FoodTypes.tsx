6bf59059de03f6bd344e63561d8624c63dffb70a
Harry Ghazni <hghazni@gmail.com>
2019-03-03 03:01:47 +0000

    Adds basic styling to Food Types

--- src/RootComponents/FoodTypes/FoodTypes.tsx
+++ src/RootComponents/FoodTypes/FoodTypes.tsx
@@ -5,14 +5,17 @@ export const FoodTypes: React.SFC = () => {
 
   return (
     <div className="food-types">
-        <h1>Food Types</h1>
-<ul className="food-types">
-  <li className="food-type">Pizza</li>
-  <li className="food-type">Chicken</li>
-  <li className="food-type">Indian</li>
-  <li className="food-type">Chinese</li>
-  <li className="food-type">English</li>
-</ul>
+      <ul className="food-type-list">
+        <li className="food-type-item">Pizza</li>
+        <li className="food-type-item">Chicken</li>
+        <li className="food-type-item">Indian</li>
+        <li className="food-type-item">Chinese</li>
+        <li className="food-type-item">English</li>
+        <li className="food-type-item">Fish & Chips</li>
+        <li className="food-type-item">Kebab</li>
+        <li className="food-type-item">Curry</li>
+        <li className="food-type-item">Carribean</li>
+      </ul>
     </div>
   )
 };

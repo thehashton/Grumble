a3f993efc08254ac9817e18751b65dbd3cf73af3
Harry Ghazni <hghazni@gmail.com>
2019-04-24 03:30:22 +0100

    Adds empty close button for modal

--- src/components/Global/Modal/Modal.tsx
+++ src/components/Global/Modal/Modal.tsx
@@ -4,10 +4,14 @@ import Modal_Inner from './Modal_Inner';
 import './modal.scss';
 
 function Modal(props: any) {
+
   return ReactDOM
     .createPortal(
-       <div className={props.class}><Modal_Inner {...props} /></div>,
-    (document.querySelector("#modal") as any as Element )
-     );
+      <div className={props.class}>
+        <span className="close-modal"></span>
+        <Modal_Inner {...props} />
+      </div>,
+      (document.querySelector("#modal") as any as Element)
+    );
 }
 export default Modal;

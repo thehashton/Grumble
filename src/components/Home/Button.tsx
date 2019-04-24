03b0a2d79b7365d94d86d9fdc22a2bc3680a9748
Harry Ghazni <hghazni@gmail.com>
2019-04-24 02:39:09 +0100

    Adds space back

--- src/components/Home/Button.tsx
+++ src/components/Home/Button.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 const Button = (props: any) => {
     return (
-     <button className={'button' + props.className} onClick={props.onClick}> {props.buttonText} </button>
+     <button className={'button ' + props.className} onClick={props.onClick}> {props.buttonText} </button>
    )}
    export default Button;

3f710e132172aec3b1e538371d0aa27275d3f1db
Harry Ghazni <hghazni@gmail.com>
2019-05-26 03:20:50 +0100

    Adds page transitions on routes now.
    Using  'react transition group'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Home from './RootComponents/Home';
 import Header from './components/Global/Header';
 import { Switch, Route } from 'react-router-dom';
@@ -7,6 +7,8 @@ import About from './RootComponents/About';
 import Account from './RootComponents/Account';
 import FAQ from './RootComponents/FAQ';
 import Contact from './RootComponents/Contact';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 class App extends Component {
   render() {
 
@@ -15,13 +17,24 @@ class App extends Component {
           <div className="App-header">
             <Header />
 
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/account" component={Account} />
-                <Route path="/faq" component={FAQ} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
+            <Route render={({location}) => (
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  timeout={200}
+                  classNames="fade"
+                  >
+                    <Switch location={location}>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/account" component={Account} />
+                        <Route path="/faq" component={FAQ} />
+                        <Route path="/contact" component={Contact} />
+                    </Switch>
+                  </CSSTransition>
+              </TransitionGroup>
+            )} />
+
           </div>
         </div>
     );

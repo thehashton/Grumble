057eb23ad1e420d2f4434267be5c3d6283d740d0
Harry Ghazni <hghazni@gmail.com>
2019-05-26 02:34:38 +0100

    Totally refactors buttons component.
    Now uses state/Dialog component instead of portals/modals for search.

--- src/components/Home/Buttons.tsx
+++ src/components/Home/Buttons.tsx
@@ -5,6 +5,7 @@ import FoodTypes from '../../RootComponents/FoodTypes';
 import Random from '../../RootComponents/Random';
 import TravelTo from '../../RootComponents/TravelTo';
 import Dialog from '../../components/Global/Dialog';
+
 export default class Buttons extends Component {
 
   state = {
@@ -14,21 +15,46 @@ export default class Buttons extends Component {
   }
 
   render() {
+
+
+    const {foodTypesOpen, randomChoiceOpen, travelToOpen} = this.state;
+
     return (
       <div>
-        <Button onClick={() => this.setState({ foodTypesOpen: true, randomChoiceOpen: false, travelToOpen: false})} className={`food-types ${this.state.foodTypesOpen ? `active` : ``}`} buttonText="Food Types"/>
-        <Button onClick={() => this.setState({ randomChoiceOpen: true, foodTypesOpen: false, travelToOpen: false})} className={`random-choice ${this.state.randomChoiceOpen ? `active` : ``}`} buttonText="Random"/>
-        <Button onClick={() => this.setState({ travelToOpen: true, randomChoiceOpen: false, foodTypesOpen: false})} className={`travel-to ${this.state.travelToOpen ? `active` : ``}`} buttonText="Travel To"/>
+        <Button
+            onClick={() => this.setState({ foodTypesOpen: true, randomChoiceOpen: false, travelToOpen: false})}
+            className={`food-types ${foodTypesOpen ? `active` : ``}`}
+            buttonText="Food Types"
+          />
+        <Button
+          onClick={() => this.setState({ randomChoiceOpen: true, foodTypesOpen: false, travelToOpen: false})}
+          className={`random-choice ${randomChoiceOpen ? `active` : ``}`}
+          buttonText="Random"
+        />
+        <Button
+          onClick={() => this.setState({ travelToOpen: true, randomChoiceOpen: false, foodTypesOpen: false})}
+          className={`travel-to ${travelToOpen ? `active` : ``}`}
+          buttonText="Travel To"
+        />
 
-        <Dialog isOpen={this.state.foodTypesOpen} onClose={() => this.setState({ foodTypesOpen: false})} dialogClass={`food-types ${this.state.foodTypesOpen ? `active` : ``}`}>
+        <Dialog
+          isOpen={this.state.foodTypesOpen}
+          onClose={() => this.setState({ foodTypesOpen: false})}
+          dialogClass={`food-types ${foodTypesOpen ? `active` : ``}`}>
           <FoodTypes />
         </Dialog>
 
-        <Dialog isOpen={this.state.randomChoiceOpen} onClose={() => this.setState({ randomChoiceOpen: false})} dialogClass={`random-choice ${this.state.randomChoiceOpen ? `active` : ``}`}>
+        <Dialog
+          isOpen={this.state.randomChoiceOpen}
+          onClose={() => this.setState({ randomChoiceOpen: false})}
+          dialogClass={`random-choice ${randomChoiceOpen ? `active` : ``}`}>
           <Random />
         </Dialog>
 
-        <Dialog isOpen={this.state.travelToOpen} onClose={() => this.setState({ travelToOpen: false})} dialogClass={`travel-to ${this.state.travelToOpen ? `active` : ``}`}>
+        <Dialog
+          isOpen={this.state.travelToOpen}
+          onClose={() => this.setState({ travelToOpen: false})}
+          dialogClass={`travel-to ${travelToOpen ? `active` : ``}`}>
           <TravelTo />
         </Dialog>
       </div>

8411fbbe5e54d8023fce6f301ff9ae29b137a1ec
Harry Ghazni <hghazni@gmail.com>
2019-05-29 02:31:28 +0100

    Dirtiest code I've ever had the mispleasure of writing.
    Account Sidebar animation activation class management.

--- src/RootComponents/Account/Account.tsx
+++ src/RootComponents/Account/Account.tsx
@@ -9,14 +9,25 @@ const wishlistIcon = '../../assets/images/wishlist-icon.svg';
 const logoutIcon = '../../assets/images/logout-icon.svg';
 
 export default class Account extends Component {
-  toggleClass = (event: any) => {
-    event.target.classList.toggle('active')
- }
 
-toggleSidebar = function() {
-  const sidebarEle = document.querySelector('.sidebar');
-  sidebarEle.classList.toggle('active');
-};
+  //TODO tidy this dirty fucking code up
+  toggleSidebar = function() {
+    const contentEle: any = document.querySelector('.Content__wrapper');
+    const sidebarEle: any = document.querySelector('.sidebar');
+
+    sidebarEle.classList.toggle('active');
+    contentEle.classList.toggle('active');
+  }
+
+  preventSideMenuItem = () => {
+    const sidebarEle: any = document.querySelector('.sidebar');
+    const contentEle: any = document.querySelector('.Content__wrapper');
+
+    sidebarEle.classList.remove('active');
+    sidebarEle.classList.add('active');
+    contentEle.classList.remove('active');
+    contentEle.classList.add('active');
+  }
 
   render() {
 
@@ -29,9 +40,8 @@ toggleSidebar = function() {
             <p>Admin content goes here</p>
           </div>
 
-
           <div
-          onClick={this.toggleClass}
+          onClick={this.toggleSidebar}
           className={`sidebar`}>
             <div className="avatar" title="avatar">
               <img src={blankAvatar}/>
@@ -41,37 +51,37 @@ toggleSidebar = function() {
             className={`sidebar-minimise`}>
             </div>
             <ul className={"side-menu"}>
-              <li className="side-menu__item" title="account">
+              <li className="side-menu__item" title="account" onClick={this.preventSideMenuItem}>
                 <span className="side-menu__icon account">
                   <img src={accountIcon} alt="account"/>
                 </span>
-                <a className="side-menu__link" href="#">My Account</a>
+                <a  className="side-menu__link" href="#">My Account</a>
                 </li>
-              <li className="side-menu__item" title="edit">
+              <li className="side-menu__item" title="edit" onClick={this.preventSideMenuItem}>
                 <span className="side-menu__icon edit">
                   <img src={editIcon} alt="edit"/>
                 </span>
                 <a className="side-menu__link" href="#">Edit Account</a>
                 </li>
-              <li className="side-menu__item" title="password">
+              <li className="side-menu__item" title="password" onClick={this.preventSideMenuItem}>
               <span className="side-menu__icon password">
                   <img src={passwordIcon} alt="password"/>
                 </span>
                 <a className="side-menu__link" href="#">Password</a>
                 </li>
-              <li className="side-menu__item" title="address">
+              <li className="side-menu__item" title="address" onClick={this.preventSideMenuItem}>
               <span className="side-menu__icon address">
                   <img src={addressIcon} alt="address"/>
                 </span>
                 <a className="side-menu__link" href="#">Address</a>
                 </li>
-              <li className="side-menu__item" title="wishlist">
+              <li className="side-menu__item" title="wishlist" onClick={this.preventSideMenuItem}>
               <span className="side-menu__icon wishlist">
                   <img src={wishlistIcon} alt="wishlist"/>
                 </span>
                 <a className="side-menu__link" href="#">Wish List</a>
                 </li>
-              <li className="side-menu__item" title="logout">
+              <li className="side-menu__item" title="logout" onClick={this.preventSideMenuItem}>
               <span className="side-menu__icon logout">
                   <img src={logoutIcon} alt="Log Out"/>
                 </span>

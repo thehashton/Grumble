947d57f569e21961a5b8fb0984dd9d2f827367a6
Harry Ghazni <hghazni@gmail.com>
2019-06-15 20:10:57 +0100

    Removes unused array

--- src/RootComponents/Home/Home.tsx
+++ src/RootComponents/Home/Home.tsx
@@ -5,13 +5,6 @@ import HeroLogo from '../../components/Home/HeroLogo';
 import HeroTitle from '../../components/Home/HeroTitle';
 
 export const Home: React.SFC = () => {
-  var msgArr = new Array();
-
-  msgArr[0] = "Hungry?";
-  msgArr[1] = "Famished?";
-  msgArr[2] = "You a starvin marvin?";
-
-  const message = msgArr[Math.floor(Math.random()*msgArr.length)];
 
   return (
     <div className={"Home"}>

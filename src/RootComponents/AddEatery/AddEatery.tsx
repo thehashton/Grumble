dade83b06e04f17f4c9cd2d258efb21085db8d28
Harry Ghazni <hghazni@gmail.com>
2019-06-15 20:31:13 +0100

    Adds addEatery component

---
+++ src/RootComponents/AddEatery/AddEatery.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AddEatery extends Component {
+    render() {
+        return (
+            <>
+                <h1>add eatery</h1>
+                <input type="text"/>
+            </>
+        )
+    }
+}
--- src/components/Home/Buttons.tsx
+++ src/components/Home/Buttons.tsx
@@ -5,35 +5,42 @@ import FoodTypes from '../../RootComponents/FoodTypes';
 import Random from '../../RootComponents/Random';
 import TravelTo from '../../RootComponents/TravelTo';
 import Dialog from '../../components/Global/Dialog';
+import AddEatery from '../../RootComponents/AddEatery';
 
 export default class Buttons extends Component {
 
   state = {
     foodTypesOpen: false,
     randomChoiceOpen: false,
-    travelToOpen: false
+    travelToOpen: false,
+    addEatery: false
   }
 
   render() {
 
-    const {foodTypesOpen, randomChoiceOpen, travelToOpen} = this.state;
+    const {foodTypesOpen, randomChoiceOpen, travelToOpen, addEatery} = this.state;
 
     return (
       <div>
         <Button
-            onClick={() => this.setState({ foodTypesOpen: true, randomChoiceOpen: false, travelToOpen: false})}
+            onClick={() => this.setState({ foodTypesOpen: true, randomChoiceOpen: false, travelToOpen: false, addEatery: false})}
             className={`food-types ${foodTypesOpen ? `active` : ``}`}
             buttonText="Food Types"
           />
         <Button
-          onClick={() => this.setState({ randomChoiceOpen: true, foodTypesOpen: false, travelToOpen: false})}
+          onClick={() => this.setState({ randomChoiceOpen: true, foodTypesOpen: false, travelToOpen: false, addEatery: false})}
           className={`random-choice ${randomChoiceOpen ? `active` : ``}`}
           buttonText="Random"
         />
         <Button
-          onClick={() => this.setState({ travelToOpen: true, randomChoiceOpen: false, foodTypesOpen: false})}
+          onClick={() => this.setState({ travelToOpen: true, randomChoiceOpen: false, foodTypesOpen: false, addEatery: false})}
           className={`travel-to ${travelToOpen ? `active` : ``}`}
           buttonText="Travel To"
+        />
+        <Button
+          onClick={() => this.setState({ travelToOpen: false, randomChoiceOpen: false, foodTypesOpen: false, addEatery: true})}
+          className={`addEatery ${addEatery ? `active` : ``}`}
+          buttonText="+"
         />
 
         <Dialog
@@ -55,6 +62,13 @@ export default class Buttons extends Component {
           onClose={() => this.setState({ travelToOpen: false})}
           dialogClass={`travel-to ${travelToOpen ? `active` : ``}`}>
           <TravelTo />
+        </Dialog>
+
+        <Dialog
+          isOpen={this.state.addEatery}
+          onClose={() => this.setState({ addEatery: false})}
+          dialogClass={`addEatery ${addEatery ? `active` : ``}`}>
+          <AddEatery />
         </Dialog>
       </div>
     )

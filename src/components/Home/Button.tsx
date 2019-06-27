d5b066bad674830fcdba4ec8c25f834b630c175a
Harry Ghazni <hghazni@gmail.com>
2019-06-27 21:17:07 +0100

    Prettifies normal components

--- src/components/Home/Button.tsx
+++ src/components/Home/Button.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Button = (props: any) => {
-    return (
-     <button className={'button ' + props.className} onClick={props.onClick}> {props.buttonText} </button>
-   )}
-   export default Button;
+  return (
+    <button className={'button ' + props.className} onClick={props.onClick}> {props.buttonText} </button>
+  )
+}
+export default Button;
--- src/components/Home/HeroTitle.tsx
+++ src/components/Home/HeroTitle.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './button.scss';
 
-export const HeroTitle: React.SFC = () => { 
+export const HeroTitle: React.SFC = () => {
 
   var msgArr = new Array();
 
@@ -9,13 +9,13 @@ export const HeroTitle: React.SFC = () => {
   msgArr[1] = "Famished?";
   msgArr[2] = "You a starvin marvin?";
 
-  const message = msgArr[Math.floor(Math.random()*msgArr.length)];
+  const message = msgArr[Math.floor(Math.random() * msgArr.length)];
 
   return (
-      <div className="home-wrapper hero">
-        <h1 id="hero-title">{message}</h1>
-        <p>Let's find you a place to eat</p>
-      </div>
+    <div className="home-wrapper hero">
+      <h1 id="hero-title">{message}</h1>
+      <p>Let's find you a place to eat</p>
+    </div>
   )
 };
 

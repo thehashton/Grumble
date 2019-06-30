61fb64891196e7e0e827dd28f9f9499ab3f581bc
Harry Ghazni <hghazni@gmail.com>
2019-06-30 22:44:35 +0100

    Adds scss variables to stylesheets with stories

--- src/components/Global/Dialog/__stories__/Dialog.stories.tsx
+++ src/components/Global/Dialog/__stories__/Dialog.stories.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import Dialog from "../Dialog";
-
 const stories = storiesOf("Dialog", module);
 stories.addDecorator(withKnobs);
 
 stories.add("Dialog/Modal", () => {
-  return (
-    <Dialog>
-      <p>lol</p>
-    </Dialog>
-  );
+  return <Dialog onClick={console.log("Dialog in Storybook")}>Dialog</Dialog>;
 });

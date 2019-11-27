2cc1a4ac1760c70fbedea006d04fd8c46c51d0f5
Harry Ghazni <hghazni@gmail.com>
2019-11-27 14:14:15 +0000

    Random eatery logic

--- src/RootComponents/Random/Random.tsx
+++ src/RootComponents/Random/Random.tsx
@@ -1,21 +1,49 @@
-import React from 'react'
-import './random.scss';
+import React from "react";
+import "./random.scss";
+import { gql } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import { client } from "../..";
+import { EateryItem } from "../Eatery/EateryItem";
+
+const EATERY_QUERY = gql`
+  {
+    eateries {
+      id
+      name
+      address
+      foodType
+    }
+  }
+`;
 
 export const Random: React.SFC = () => {
-
   return (
     <div className="random">
-      <h1>Random</h1>
-      <div className="choice">
-        <div className="choice__image"></div>
-        <div className="choice__content">
-          <h3 className="choice__title">Munch Munch</h3>
-          <p className="choice__type">Chicken wings and stuff</p>
-          <span className="rating">4/5 stars</span>
-        </div>
-      </div>
+      <ApolloProvider client={client}>
+        <Query query={EATERY_QUERY}>
+          {({ loading, data }: any) => {
+            if (loading) return "Loading...";
+            const { eateries } = data;
+            const randomItem = function(obj: any) {
+              var keys = Object.keys(obj);
+              return obj[keys[(keys.length * Math.random()) << 0]];
+            };
+            const randomEatery = randomItem(eateries);
+
+            return (
+              <EateryItem
+                key={randomEatery.id}
+                id={randomEatery.id}
+                name={randomEatery.name}
+                address={randomEatery.address}
+                foodType={randomEatery.foodType}
+              />
+            );
+          }}
+        </Query>
+      </ApolloProvider>
     </div>
-  )
+  );
 };
 
 export default Random;

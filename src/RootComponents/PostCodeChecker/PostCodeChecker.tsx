3e0e2e7366b5f7f5a21fc4be42a230e95833fb0a
Harry Ghazni <hghazni@gmail.com>
2019-12-12 01:50:42 +0000

    Shapes postcode API response

--- src/RootComponents/PostCodeChecker/PostCodeChecker.tsx
+++ src/RootComponents/PostCodeChecker/PostCodeChecker.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { Dispatch } from "redux";
 import "./PostCodeChecker.scss";
 import axios from "axios";
 import store from "../../../runtime/store/store";
@@ -8,10 +6,11 @@ import { setPostCode } from "../../../runtime/store/actions";
 
 type PostCodeCheckerProps = {
   postCode?: string;
+  userLocation?: {};
 };
 
 export const PostCodeChecker: React.SFC<PostCodeCheckerProps> = props => {
-  const dispatch = useDispatch<Dispatch<any>>();
+  const usersPostcode = localStorage.getItem("postCode");
   const [state, setState] = React.useState({
     userPostCode: ""
   });
@@ -24,14 +23,24 @@ export const PostCodeChecker: React.SFC<PostCodeCheckerProps> = props => {
     event.preventDefault();
     store.dispatch(setPostCode(state.userPostCode));
     localStorage.setItem("postCode", state.userPostCode);
-    console.log(localStorage.getItem("postCode"));
   }
 
-  // Make a request for a postcode
+  let userLocObj: any = {};
   axios
-    .get("https://api.postcodes.io/postcodes/" + "NG3 7FX")
-    .then(function(response) {
+    .get("https://api.postcodes.io/postcodes/" + usersPostcode)
+    .then(function(userLocation) {
       // handle success
+      const usrLocData = userLocation.data.result;
+      const userLocationRes = {
+        userLocation: {
+          lat: usrLocData.latitude,
+          long: usrLocData.longitude,
+          city: usrLocData.nuts,
+          region: usrLocData.region,
+          country: usrLocData.country
+        }
+      };
+      Object.assign(userLocObj, userLocationRes);
     })
     .catch(function(error: any) {
       // handle error

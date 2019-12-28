54780020eed2ff9dbe44d9512fec7b8d00e9fc47
Harry Ghazni <hghazni@gmail.com>
2019-12-28 23:45:52 +0000

    Conditional rendering on home

--- src/RootComponents/Home/Home.tsx
+++ src/RootComponents/Home/Home.tsx
@@ -6,19 +6,19 @@ import HeroLogo from "../../components/Home/HeroLogo";
 import HeroTitle from "../../components/Home/HeroTitle";
 import Search from "../../components/Search/Search";
 import PostCodeChecker from "../../RootComponents/PostCodeChecker";
-
 export const Home: React.SFC = () => {
   return (
     <div className={"Home"}>
-      <div className={"Home__column-left"}>
-        <HeroTitle />
-        <HeroLogo />
-      </div>
-      <div className={"Home__column-right"}>
-        <Search />
-        <Buttons />
+      {localStorage.getItem("postCode") === "" ? (
+        <>
+          <HeroTitle />
+          <HeroLogo />
+          <Search />
+          <Buttons />
+        </>
+      ) : (
         <PostCodeChecker />
-      </div>
+      )}
     </div>
   );
 };

a07867b966a84e0e370484022769ad23bdd9cb82
Harry Ghazni <hghazni@gmail.com>
2019-12-31 23:15:35 +0000

    Adds user lat long to redux store

--- runtime/store/constants/action-types.ts
+++ runtime/store/constants/action-types.ts
@@ -2,5 +2,7 @@ export const SUBMIT_EATERY = "ADD_ARTICLE";
 export const SET_POSTCODE = "SET_POSTCODE";
 export const SET_EATERY_POSTCODE = "SET_EATERY_POSTCODE";
 export const SET_EATERY_ADDRESS = "SET_EATERY_ADDRESS";
+export const SET_USER_LAT = "SET_USER_LAT";
+export const SET_USER_LONG = "SET_USER_LONG";
 
 

50c00450f11f735565bb16d875097b6d509b5e1b
Harry Ghazni <hghazni@gmail.com>
2020-01-12 21:17:36 +0000

    Adds wrapper for header

--- src/components/Global/Header/Header.tsx
+++ src/components/Global/Header/Header.tsx
@@ -6,41 +6,43 @@ import LightDarkToggle from "../LightDarkToggle";
 
 const Header = (props: any) => {
   return (
-    <header className={"header"}>
-      <div className={"logo"}>
-        <NavLink exact to="/" activeClassName="active">
-          <h1 className="logoText">Grumble</h1>
-        </NavLink>
-      </div>
-      <LightDarkToggle />
-      {/* <ul className={"menu"}>
-        <li className={"menu__item"}>
-          <NavLink
-            exact
-            to="/about"
-            className={"menu__link"}
-            activeClassName="active"
-          >
-            About
+    <div className="headerWrapper">
+      <header className={"header"}>
+        <div className={"logo"}>
+          <NavLink exact to="/" activeClassName="active">
+            <h1 className="logoText">Grumble</h1>
           </NavLink>
-        </li>
-        <li className={"menu__item"}>
-          <NavLink to="/account" className={"menu__link"}>
-            Account
-          </NavLink>
-        </li>
-        <li className={"menu__item"}>
-          <NavLink to="/faq" className={"menu__link"}>
-            FAQ
-          </NavLink>
-        </li>
-        <li className={"menu__item"}>
-          <NavLink to="/contact" className={"menu__link"}>
-            Contact
-          </NavLink>
-        </li>
-      </ul> */}
-    </header>
+        </div>
+        <LightDarkToggle />
+        {/* <ul className={"menu"}>
+          <li className={"menu__item"}>
+            <NavLink
+              exact
+              to="/about"
+              className={"menu__link"}
+              activeClassName="active"
+            >
+              About
+            </NavLink>
+          </li>
+          <li className={"menu__item"}>
+            <NavLink to="/account" className={"menu__link"}>
+              Account
+            </NavLink>
+          </li>
+          <li className={"menu__item"}>
+            <NavLink to="/faq" className={"menu__link"}>
+              FAQ
+            </NavLink>
+          </li>
+          <li className={"menu__item"}>
+            <NavLink to="/contact" className={"menu__link"}>
+              Contact
+            </NavLink>
+          </li>
+        </ul> */}
+      </header>
+    </div>
   );
 };
 export default Header;

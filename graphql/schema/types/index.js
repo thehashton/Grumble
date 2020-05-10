4e32b327fec32bbbb8e756175efd8bca1f209987
Harry Ghazni <hghazni@gmail.com>
2020-05-10 16:54:12 +0100

    Paff

--- graphql/schema/types/index.js
+++
@@ -1,8 +0,0 @@
-const { mergeTypes } = require("merge-graphql-schemas");
-
-const User = require("./User/");
-const Eatery = require("./Eatery/");
-
-const typeDefs = [User, Eatery];
-
-module.exports = mergeTypes(typeDefs, { all: true });
